428e7c6e7b5165c7212bb4b279a148e8f63953a9
Haley Williams <53011994+haleyw1237@users.noreply.github.com>
2023-03-15 12:43:36 -0500

    Add files via upload

---
+++ gradebook.py
@@ -0,0 +1,32 @@
+# gradebook.py
+
+
+
+
+gradebook = [[61, 74, 69, 62, 72, 66, 73, 65, 60, 63, 69, 63, 62, 61, 64],
+             [73, 80, 78, 76, 76, 79, 75, 73, 76, 74, 77, 79, 76, 78, 72],
+             [90, 92, 93, 92, 88, 93, 90, 95, 100, 99, 100, 91, 95, 99, 96],
+             [96, 89, 94, 88, 100, 96, 93, 92, 94, 98, 90, 90, 92, 91, 94],
+             [76, 76, 82, 78, 82, 76, 84, 82, 80, 82, 76, 86, 82, 84, 78],
+             [93, 92, 89, 84, 91, 86, 84, 90, 95, 86, 88, 95, 88, 84, 89],
+             [63, 66, 55, 67, 66, 68, 66, 56, 55, 62, 59, 67, 60, 70, 67],
+             [86, 92, 93, 88, 90, 90, 91, 94, 90, 86, 93, 89, 94, 94, 92],
+             [89, 80, 81, 89, 86, 86, 85, 80, 79, 90, 83, 85, 90, 79, 80],
+             [99, 73, 86, 77, 87, 99, 71, 96, 81, 83, 71, 75, 91, 74, 72]]
+
+# Display the average of each student's grade. 
+def get_student_average(gradebook):
+        
+         return  [sum(row)/len(row) for row in gradebook ]
+
+for i, Average in enumerate(get_student_average(gradebook)):
+    print(f"Student Average {i+1}: {Average:.4} ")
+print("")
+
+#Display the average for each assignment. 
+def get_assignment_average(gradebook):
+    
+    return [ sum(x)/len(gradebook) for x in zip(*gradebook) ]
+
+for i, Average in enumerate(get_assignment_average(gradebook)):
+    print(f"Assignment Average {i+1}: {Average:.4}")
